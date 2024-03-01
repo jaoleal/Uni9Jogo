345f32567b1404b1e663dc32102dc3e62d1fc1ba
joaoleal <devjoaoleal1006@gmail.com>
2024-03-01 16:55:14 -0300

    Add: no-logic: Scene Char and Comp

---
+++ easy-mode/godot-logic/Scenes/Chars/KinematicBody2D.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var _gravity = _get("gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(_gravity)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ easy-mode/godot-logic/Scenes/Comps/Test_Comps.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+#Define Composition Gravity
+export var gravity = 40
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
